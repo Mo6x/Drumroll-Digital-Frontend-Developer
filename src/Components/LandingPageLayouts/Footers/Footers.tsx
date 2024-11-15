786f00d46e109e9d2edaa2ff17f2f224647f0c89
mo6x <christmoses1999@gmail.com>
2024-11-15 21:38:53 +0100

    working

--- src/Components/LandingPageLayouts/Footers/Footers.tsx
+++ src/Components/LandingPageLayouts/Footers/Footers.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import Styles from "./Footers.module.css";
 
+
+
 export const Footers: React.FC = () => {
   return (
     <div className={Styles.FootersContainer}>
+      <div>
+        <div>
+          <h1>Contact Information</h1>
+          <p>Please reach out to us if you have any questions</p>
+           <div>
+              <div>
 
-      
+                 <h4>+234 (916) 984-5070</h4>
+              </div>
+           </div>
+        </div>
+      </div>
+
       <form className={Styles.Form}>
         <div className={Styles.InputRow}>
           <input type="text" placeholder="Full Name" className={Styles.Input} />
